0bb1a33bc27fc2a06d15738454dcbb408cd34d0d
jkarenas <jc.arenas1993@gmail.com>
2023-10-22 14:42:12 -0500

    deploy

--- src/server.js
+++ src/server.js
@@ -9,6 +9,12 @@ server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
 
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 server.use(router);
 
 module.exports = server;
